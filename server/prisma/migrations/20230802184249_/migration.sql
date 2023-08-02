8306a38a242c6557b218e4bff497b1c3777ee092
Claudio <claudio1419teixeira2020@gmail.com>
2023-08-02 15:43:51 -0300

    change migrtations

---
+++ server/prisma/migrations/20230802184249_/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "avatarUser" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Birthday" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "personName" TEXT NOT NULL,
+    "dateOfBirth" TIMESTAMP(3) NOT NULL,
+    "avatarBirthday" TEXT NOT NULL,
+
+    CONSTRAINT "Birthday_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- AddForeignKey
+ALTER TABLE "Birthday" ADD CONSTRAINT "Birthday_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
