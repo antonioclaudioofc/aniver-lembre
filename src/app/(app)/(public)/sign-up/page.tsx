6aeacabfbc22b85ec27f49a71e6244d69d600d31
Antonio Claudio <claudioalvesdev@gmail.com>
2025-03-19 11:13:30 -0300

    feat: create page signIn

--- src/app/(app)/(public)/sign-up/page.tsx
+++ src/app/(app)/(public)/sign-up/page.tsx
@@ -1,3 +1,37 @@
+import Image from "next/image";
+import logo from "@/assets/logo.svg";
+import { Label } from "@/components/Label";
+import { Input } from "@/components/Input";
+import { Button } from "@/components/Button";
+
 export default function SignUp() {
-  return <h1>SignUp</h1>;
+  return (
+    <div className="max-w-96">
+      <div className="max-w-24 max-h-24 bg-gray-100 shadow p-2 rounded-full mx-auto mb-2">
+        <Image
+          src={logo}
+          className="w-full h-full "
+          alt="Logo do site, que é um calendário com cor roxo"
+        />
+      </div>
+      <h1 className="text-5xl font-bold text-center">AniverLembre</h1>
+      <p className="text-gray-500 text-center">
+        Preencha as informações abaixo!
+      </p>
+      <form action="#" method="post" className="grid grid-cols-1 gap-2 mt-4">
+        <div>
+          <Label>E-mail</Label>
+          <Input placeholder="Digite seu e-mail" />
+        </div>
+        <div>
+          <Label>Senha</Label>
+          <Input placeholder="Digite sua senha" />
+        </div>
+        <span className="text-right text-sm text-indigo-400 font-medium cursor-pointer transition-colors hover:text-indigo-500">
+          Esqueceu a senha?
+        </span>
+        <Button>Entrar</Button>
+      </form>
+    </div>
+  );
 }
---
+++ src/components/Button.tsx
@@ -0,0 +1,49 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const buttonVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-base font-semibold transition-colors outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0 px-5 py-3 max-h-12",
+  {
+    variants: {
+      variant: {
+        default:
+          "bg-indigo-500 text-white border-indigo-600 hover:bg-indigo-600",
+        destructive: "bg-red-500 text-white hover:bg-red-500/80",
+        outline:
+          "bg-white border border-indigo-500 text-black hover:text-gray-100",
+        secondary:
+          "bg-green-500 text-white border-green-600 hover:bg-green-600",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "text-primary underline-offset-4 hover:underline",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+    },
+  }
+);
+
+export interface ButtonProps
+  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+    VariantProps<typeof buttonVariants> {
+  asChild?: boolean;
+}
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ className, variant, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
+    return (
+      <Comp
+        className={cn(buttonVariants({ variant, className }))}
+        ref={ref}
+        {...props}
+      />
+    );
+  }
+);
+Button.displayName = "Button";
+
+export { Button, buttonVariants };
